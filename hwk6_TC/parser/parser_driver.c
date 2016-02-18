251c797de1dfb9956ba042103b8845fff3c02aaf
Seok Jun Bing <jun@juns-mbp.wireless.dartmouth.edu>
2016-02-18 16:08:58 -0500

    again

--- hwk6_TC/parser/parser_driver.c
+++ hwk6_TC/parser/parser_driver.c
@@ -39,6 +39,7 @@ int main(int argc, char *argv[]) {
  
 if(argc != 2) {
 	fprintf(stderr, "Please input the ourput file name\n");
+	return 1;
 }
 
  int noRoot = 0;		/* 0 means we will have a root */
