ac2f8f051a307a1c6ca35fbb91f89a6c40401bd7
Juan C. Torres <juan.c.torres.ramirez.17@dartmouth.edu>
2016-02-23 19:27:44 -0500

    improved test8.c

--- hwk6_TC/parser/testinputs/test8.c
+++ hwk6_TC/parser/testinputs/test8.c
@@ -14,21 +14,47 @@
 // }
 
 
-
 int main() {
 
 
 	int a[12];
+	int b;
+	int c;
+	int d;
+	//a[0] = 7;
+	//a[1] = 2;
+	//a[2] = 3;
 
-	a[0] = 7;
-	a[1] = 2;
-	a[2] = 3;
+	b = 2;
+	c = 3;
+	d = 2;
 
-	print a[0];
-	print a[1];
-	print a[2];
+	// print "Before\n";
+	// print d;
 
+	a[4] = 2;
+
+	d = (a[4] == 2);
+
+	print d;
+	// if(a[4] == ){
+	// 	print c;
+	// }
+	// ++a[4];
+	//c = b + a[4];
+	//print a[4];
+	// print c;
+
+	// print "\nAfter\n";
+
+	// d = c + a[4];
+
+	// print d;
+	//print a[0];
+	//print a[1];
+	// print a[2];
+
+	//return a[4];
 	return 0;
-
-
+	//return a[4];
 }
